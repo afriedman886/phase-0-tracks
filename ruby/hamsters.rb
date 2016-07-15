e88c1d1c28aab1d3cae8cf6eecd02deb4ef2eb19
Amy Friedman <afriedman886@gmail.com>
2016-07-15 15:54:52 -0500

    change order of final hamster statements

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,18 +1,23 @@
-puts "What is the hamster's name?"
-name = gets.chomp
-puts "What is the volume level of your hamster? (1-10)"
-volume = gets.chomp
-puts "What is the hamster's fur color?"
-color = gets.chomp
-puts "Is the hamster a good candidate for adoption? (y/n)"
-adoption = gets.chomp
-puts "What is your hamster's estimated age?"
-age = gets.chomp
+def hamster
+  puts "What is the hamster's name?"
+  name = gets.chomp
+  puts "How loud is the hamster? (Please rate the volume on a scale of 1-10.)"
+  volume = gets.chomp.to_i
+  puts "What is the hamster's fur color?"
+  color = gets.chomp
+  puts "Is the hamster a good candidate for adoption? (yes/no)"
+  adoption = gets.chomp
+  puts "What is your hamster's estimated age?"
+  age = gets.chomp
 
-def age(hamster)
-  if age == nil
-    age = nil
-  end
+    if age.empty?
+      age = "nil"
+    else age = gets.chomp.to_i
+    end
+
+  puts "Hamster's name: #{name}"
+  puts "Hamster's estimated age: #{age}"
+  puts "Fur color: #{color}"
+  puts "Volume level: #{volume}"
+  puts "Good candidate for adoption: #{adoption}"
 end
-
-puts "#{age}"
