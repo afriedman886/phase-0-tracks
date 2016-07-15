1ae47f0eabf4633f7b1284cf36d234b26c026984
Amy Friedman <afriedman886@gmail.com>
2016-07-15 16:21:51 -0500

    final syntax cleanup

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,25 +1,22 @@
-
-  puts "What is the hamster's name?"
-  name = gets.chomp
-  puts "How loud is the hamster? (Please rate the volume on a scale of 1-10.)"
-  volume = gets.chomp.to_i
-  puts "What is the hamster's fur color?"
-  color = gets.chomp
-  puts "Is the hamster a good candidate for adoption? (yes/no)"
-  adoption = gets.chomp
-  puts "What is your hamster's estimated age?"
-  age = gets.chomp
-
-    if age.empty?
-      age = nil
-    else age = age.to_i
-    end
-
-  puts "-------------------------------------------"
-  puts "Hamster's name: #{name}"
-  puts "Estimated age: #{age}"
-  puts "Fur color: #{color}"
-  puts "Volume level: #{volume}"
-  puts "Good candidate for adoption: #{adoption}"
-  puts "-------------------------------------------"
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "How loud is the hamster? (Please rate the volume on a scale of 1-10.)"
+volume = gets.chomp.to_i
+puts "What is the hamster's fur color?"
+color = gets.chomp
+puts "Is the hamster a good candidate for adoption? (yes/no)"
+adoption = gets.chomp
+puts "What is your hamster's estimated age?"
+age = gets.chomp
+  if age.empty?
+    age = nil
+  else age = age.to_i
+  end
+puts "-------------------------------------------"
+puts "Hamster's name: #{name}"
+puts "Estimated age: #{age}"
+puts "Fur color: #{color}"
+puts "Volume level: #{volume}"
+puts "Good candidate for adoption: #{adoption}"
+puts "-------------------------------------------"
 
