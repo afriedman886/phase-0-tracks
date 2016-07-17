81566552be1c5c0d9f8225f4bd8d19605e55b505
Amy Friedman <afriedman886@gmail.com>
2016-07-17 01:01:04 -0500

    add allergy questions, break loop for 'sunshine' answer

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,6 +2,8 @@ puts "How many new employees need to be processed?"
 new_employees = gets.chomp.to_i
 
 processed_new_employees = 0
+current_year = 2016
+vampire_status = nil
 
 until processed_new_employees == new_employees
 
@@ -11,22 +13,29 @@ until processed_new_employees == new_employees
   age = gets.chomp.to_i
   puts "What year were you born?"
   birth_year = gets.chomp.to_i
-  puts "We're placing orders for garlic bread, would you like some for lunch? ( yes/no)"
+  puts "We're placing orders for garlic bread, would you like some for lunch? (yes/no)"
   garlic = gets.chomp
   puts "Would you like to enroll in our company insurance plan? (yes/no)"
   insurance = gets.chomp
+  puts "Do you have any alergies? If so, please list them one at a time. (When finished, please type \"done\".)"
+  alergies = gets.chomp
+    until alergies.include?("done")
+      puts "Additional alergy?"
+      alergies = alergies + ", " + gets.chomp
+        if alergies.include?("sunshine")
+          break
+        end
+    end
 
-  vampire_status = nil
-
-    if (2016 - birth_year) == age && (garlic == "yes" || insurance == "yes")
+    if (current_year - birth_year) == age && (garlic == "yes" || insurance == "yes")
       vampire_status = "Probably not a vampire."
     end
 
-    if (2016 - birth_year) != age && (garlic == "no" || insurance == "no")
+    if ((current_year - birth_year) != age && (garlic == "no" || insurance == "no")) || alergies.include?("sunshine")
       vampire_status = "Probably a vampire."
     end
 
-    if (2016 - birth_year) != age && garlic == "no" && insurance == "no"
+    if (current_year - birth_year) != age && garlic == "no" && insurance == "no"
       vampire_status = "Almost certainly a vampire."
     end
 
