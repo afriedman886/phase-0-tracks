212f7c2d7cd931bae8c04bde04eb6a506eaeb3a6
Amy Friedman <afriedman886@gmail.com>
2016-07-16 23:12:04 -0500

    add conditional statements to determine vampire likelihood

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,15 +4,27 @@ puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
-puts "We're placing orders for garlic bread, would you like some for lunch?"
+puts "We're placing orders for garlic bread, would you like some for lunch? (yes/no)"
 garlic = gets.chomp
-  if garlic == "yes"
-    puts "Great, I'm sure you'll love it!"
-  elsif garlic == "no"
-    puts "Hmm... not a fan of garlic bread, huh?  That's... too bad."
-  else puts "I'm sorry, I didn't quite understand.  Would you like some garlic bread (yes/no)?"
-    garlic = gets.chomp
-  end
-puts "Would you like to enroll in our company insurance plan?"
+puts "Would you like to enroll in our company insurance plan? (yes/no)"
 insurance = gets.chomp
+
+case
+  when (2016 - birth_year) == age && (garlic == "yes" || insurance == "yes")
+    puts "Probably not a vampire."
+  when (2016 - birth_year) != age && (garlic == "no" || insurance == "no")
+    puts "Probably a vampire."
+  when (2016 - birth_year) != age && garlic == "no" && insurance == "no"
+    puts "Almost certainly a vampire."
+  when name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire."
+  else puts "Results inconclusive."
+end
+
+
+
+
+
+
+
 
