9b572fd2236ee8858ed6c93cc5515f4fc5866892
Amy Friedman <afriedman886@gmail.com>
2016-07-17 23:27:04 -0500

    move method defs to top of program

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,25 +1,24 @@
-# Password Encryption program
+# Password Encryption Program
 
-# ask secret agent for password, and whether they would like to encrypt or decrypt
+# password encryption: advance each letter of string one letter forward in the alphabet
+
+def encrypt(str)
+   str.tr!('a-z', 'b-za')
+end
+
+# password decryption: move each letter of string one letter backward in the alphabet
+
+def decrypt(str)
+   str.tr!('a-z', 'za-y')
+end
+
+# ask secret agent's encryption/decryption preference and what their password is
 
 puts "Would you like to encrypt or decrypt a password? (e/d)"
 response = gets.chomp
 
 puts "What is your password?"
 password = gets.chomp
-
-
-# encryption method: advance each letter of password one letter forward in the alphabet
-
-def encrypt(str)
-   str.tr!('a-z', 'b-za')
-end
-
-# decryption method: move each letter of password one letter backward in the alphabet
-
-def decrypt(str)
-   str.tr!('a-z', 'za-y')
-end
 
 # print password alteration depending on agent's encryption/decryption choice
 
@@ -29,8 +28,15 @@ else response == "d" || response == "decrypt"
     puts decrypt(password)
 end
 
+# test of encryption/decryption methods
 
-# to decrypt an encrypted password (ex: swordfish)
+puts encrypt("abc")  # should return "bcd"
+puts encrypt("zed")  # should return "afe"
+puts decrypt("bcd")  # should return "abc"
+puts decrypt("afe")  # should return "zed"
+
+
+# how to decrypt an encrypted password
 
 puts decrypt(encrypt("swordfish"))
 
