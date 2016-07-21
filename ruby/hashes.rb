01119c9b0da2ca0a7c49b77f5311d040c8daf441
Amy Friedman <afriedman886@gmail.com>
2016-07-21 02:40:02 -0500

    add client verification questions for symbols with array data, cleanup boolean verification

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -27,18 +27,18 @@ client_info[:rooms_to_decorate] = rooms_to_decorate
 
 # Assess client's budgetary contraints
 
-until budget == "y".downcase || budget == "n".downcase
+until budget == "y" || budget == "n"
 puts "Does the client have a specific budget for the project? (y/n)"
-  budget = gets.chomp
-  if budget == "y".downcase
+  budget = gets.chomp.downcase
+  if budget == "y"
     client_info[:budget] = true
-  elsif budget == "n".downcase
+  elsif budget == "n"
     client_info[:budget] = false
   else puts "I'm sorry, I didn't understand your answer."
   end
 end
 
-if budget == "y".downcase
+if budget == "y"
   puts "What is the maximum the client is willing to spend on this project? (Please do not include any symbols)"
   client_info[:max_budget] = gets.chomp.to_i
 end
@@ -51,31 +51,37 @@ puts "Would you like to make any changes? (Type 'none' if information is accurat
 errors = gets.chomp
 
 if errors != "none"
-puts "Which field name would you like to update?"
-field_name = gets.chomp.to_sym
-end
+  puts "Which field name would you like to update?"
+  field_name = gets.chomp.to_sym
 
-if field_name != :rooms_to_decorate
-puts "Please type the revised information for this field:"
+  if field_name != :rooms_to_decorate
+    puts "Please type the revised information for this field:"
 
-# Client info corrections - data type: string
-if field_name == :name || field_name == :decor
-  client_info[field_name] = gets.chomp
+    # Client info corrections - data type: string
+    if field_name == :name || field_name == :decor
+      client_info[field_name] = gets.chomp
 
-# Client info corrections - data type: integer
-elsif field_name == :age || field_name == :children || field_name == :max_budget
-  client_info[field_name] = gets.chomp.to_i
+    # Client info corrections - data type: integer
+    elsif field_name == :age || field_name == :children || field_name == :max_budget
+      client_info[field_name] = gets.chomp.to_i
 
-# Client info corrections - data type: boolean
-else field_name == :budget
-  if client_info[:budget] == true
-    client_info[:budget] = false
-  else client_info[:budget] == false
-    client_info[:budget] = true
+    # Client info corrections - data type: boolean
+    else field_name == :budget
+      client_info[:budget] = !client_info[:budget]
+    end
+
+  # Client info corrections - data type: array
+  else puts "Would you like to add (a) or remove (r) an exisiting room?"
+    room_edit = gets.chomp.downcase
+    if room_edit == "a" || room_edit == "add"
+      puts "What room would you like to add?"
+      rooms_to_decorate << gets.chomp
+    else room_edit == "r" || room_edit == "remove"
+      puts "What room would you like to remove?"
+      rooms_to_decorate.delete(gets.chomp)
+    end
   end
+
 end
-end
 
-
-
-
+puts client_info
