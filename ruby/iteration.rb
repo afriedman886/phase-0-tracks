f8bd2fee9dfc2eaa3179f9d775a11081f7261723
Amy Friedman <afriedman886@gmail.com>
2016-07-21 14:27:02 -0500

    add hollywood hash and animal array; add each method to animal array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,21 @@ end
 
 
 vacation { |city1, city2| puts "But I would like to go to either #{city1} or #{city2}." }
+
+
+animals = ["elephant", "zebra", "lion", "hippo"]
+
+hollywood = {
+  :die_hard => "Bruce Willis",
+  :indiana_jones => "Harrison Ford",
+  :forrest_gump => "Tom Hanks",
+  :iron_man => "Robert Downey Jr."
+}
+
+puts "Before .each"
+p animals
+
+animals.each { |animal| puts animal }
+
+puts "After .each"
+p animals
