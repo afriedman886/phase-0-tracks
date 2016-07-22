cf7a2dc9faacbf34b079d475049369820d4d8519
Amy Friedman <afriedman886@gmail.com>
2016-07-21 20:22:23 -0500

    add grocery list array to each planned meal for the week

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,35 +2,56 @@ weekly_meal_plan = {
   monday: {
     plan: {
       meal: "beef wellington",
-      grocery_list: "groceries"
+      grocery_list: [
+        "beef tenderloin",
+        "mushrooms",
+        "shallots"
+      ]
       },
     reality: "frozen pizza"
     },
   tuesday: {
     plan: {
       meal: "paella",
-      grocery_list: "groceries"
+      grocery_list: [
+        "prawns",
+        "sea scallops",
+        "tomato puree"
+      ]
       },
     reality: "boxed mac and cheese"
     },
   wednesday: {
     plan: {
       meal: "wild mushroom risotto",
-      grocery_list: "groceries"
+      grocery_list: [
+        "risotto rice",
+        "wild mushrooms",
+        "mascarpone"
+      ]
       },
     reality: "thai takeout"
     },
   thursday: {
     plan: {
       meal: "chicken marsala",
-      grocery_list: "groceries"
+      grocery_list: [
+        "chicken breasts",
+        "chicken broth",
+        "sundried tomatoes",
+        "marsala wine"
+      ]
       },
     reality: "peanut butter and jelly sandwich"
     },
   friday: {
     plan: {
       meal: "duck a l'orange",
-      grocery_list: "groceries"
+      grocery_list: [
+        "duck",
+        "oranges",
+        "fresh thyme"
+      ]
       },
     reality: "happy hour appetizers"
   }
