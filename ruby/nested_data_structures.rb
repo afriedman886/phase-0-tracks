9d3de970847802cde6cebccba7a29110d4426580
Amy Friedman <afriedman886@gmail.com>
2016-07-21 19:39:01 -0500

    add framing for weekly meal plan, include days of week, plan vs reality fieldnames for each day

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,37 @@
+weekly_meal_plan = {
+  monday: {
+    plan: {
+      meal:
+      groceries_to_buy:
+      }
+    reality: "frozen pizza"
+    },
+  tuesday: {
+    plan: {
+      meal:
+      groceries_to_buy:
+      }
+    reality: "boxed mac and cheese"
+    },
+  wednesday: {
+    plan: {
+      meal:
+      groceries_to_buy:
+      }
+    reality: "Thai take-out"
+    },
+  thursday: {
+    plan: {
+      meal:
+      groceries_to_buy:
+      }
+    reality: "cereal"
+    },
+  friday: {
+    plan: {
+      meal:
+      groceries_to_buy:
+      }
+    reality: "happy hour appetizers"
+  }
+}
