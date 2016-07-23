9b9c0dc44b494b648f035ed716223b49654bbcd0
Amy Friedman <afriedman886@gmail.com>
2016-07-22 23:29:01 -0500

    change .next to .tr to handle names with 'z'; include .downcase method to catch any capital vowels or capial 'Z' in name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,22 +1,32 @@
 # Fake name creator
 
 def incognito(name)
-  name_array = name.split(' ').reverse.join(' ').chars
-    i = 0
-    shift = []
-    while i < name_array.length
-      if "aeiou".include?(name_array[i])
-        shift << name_array[i].tr('aeiou', 'eioua')
-      elsif name_array[i] == " "
-        shift << " "
-      else
-        shift << name_array[i].next
-      end
-    i += 1
+  # Swap first and last name
+  # Transform the string into an array
+  name_array = name.downcase.split(' ').reverse.join(' ').chars
+  # Loop through array to alter each letter value
+  # Push each new value to new array: shift
+  i = 0
+  shift = []
+  while i < name_array.length
+    # If array value is a vowel, translate to the next vowel in sequence 'aeiou'
+    if "aeiou".include?(name_array[i])
+      shift << name_array[i].tr('aeiou', 'eioua')
+    # Keep space value constant (if provided in argument)
+    elsif name_array[i] == " "
+      shift << " "
+    # Shift all consonant letters to the next in alphabetical order
+    else
+      shift << name_array[i].tr('a-z', 'b-za')
     end
-    shift.join('')
+  i += 1
+  end
+  # Return the new array as a string
+  shift.join('').capitalize
 end
 
+# Driver Code
 
-p incognito("amy friedman")
+p incognito("Zhomasz")
+
 
