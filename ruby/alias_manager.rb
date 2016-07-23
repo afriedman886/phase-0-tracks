92dc6d663c5280f06492ae26919ba029b8171b94
Amy Friedman <afriedman886@gmail.com>
2016-07-22 22:43:26 -0500

    create method that shifts all letters to the next in alphabet

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,6 +4,21 @@ def reverse(name)
   name.split(' ').reverse.join(' ')
 end
 
+def letter_shift(name)
+  name_array = name.chars
+    i = 0
+    shift = []
+    while i < name_array.length
+
+        shift << name_array[i].next
+
+    i += 1
+    end
+    shift
+end
+
 
 
 p reverse("amy friedman")
+
+p letter_shift("amy friedman")
