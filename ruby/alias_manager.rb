70189d07205cd35f3f036314b48eb7085161d875
Amy Friedman <afriedman886@gmail.com>
2016-07-23 03:30:38 -0500

    add condition to handle consonants that occur before a vowel in alphabetical order; make sure both first and last names are capitalized in final return

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,29 +14,29 @@ def incognito(name)
     # If array value is a vowel, translate to the next vowel in sequence 'aeiou'
     if "aeiou".include?(name_array[i])
       shift << name_array[i].tr('aeiou', 'eioua')
+    # Create special rule for consonants that immediately precede a vowel in alphabetical order
+    elsif "zdhnt".include?(name_array[i])
+      shift << name_array[i].tr('zdhnt', 'bfjpv')
     # Keep space value constant (if provided in argument)
     elsif name_array[i] == " "
       shift << " "
-    # Shift all consonant letters to the next in alphabetical order; shift z => a
+    # Shift all remaining consonant letters to the next consonant in alphabetical order
     else
-      shift << name_array[i].tr('a-z', 'b-za')
+      shift << name_array[i].next
     end
   i += 1
   end
-  # Return the new array as a string
-  shift.join('').capitalize
+  # Return the new array as a string, capitalize first & last name
+  shift.join('').split(' ').map{|name| name.capitalize}.join(' ')
 end
 
 # Driver Code
 
-p incognito("Felicia Torres")
+puts incognito("Felicia Torres")
 # Should return "Vussit Gimodoe"
 
 # last name capitalize
 
-# consonant shift to next consonant (can't be vowel)
-
-# edge cases??
 
 
 puts "Enter a name to receive a super secret alias identity."
