be1193247648e0f9019179fb941b7c85a1ad516c
Amy Friedman <afriedman886@gmail.com>
2016-07-22 22:57:02 -0500

    add condition that shifts vowels from 'aeiou' to 'eioua')

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,9 +9,13 @@ def letter_shift(name)
     i = 0
     shift = []
     while i < name_array.length
-
+      if "aeiou".include?(name_array[i])
+        shift << name_array[i].tr('aeiou', 'eioua')
+      elsif name_array[i] == " "
+        shift << " "
+      else
         shift << name_array[i].next
-
+      end
     i += 1
     end
     shift
