2637c8c2a731e9983affbfc8be50596a4695627f
Amy Friedman <afriedman886@gmail.com>
2016-07-25 23:24:18 -0500

    comment out characteristics/behavior of puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#SPECIES ------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Eyes: large
+#Ears: floppy
+#Friendly: true
+#Name: varies
+#Color: varies
+
+#BEHAVIOR -----------------------
+#Bark
+#Fetch
+#Wag tail
