d11f06983a9bf9ef49288c9561013eb1ce123422
Amy Friedman <afriedman886@gmail.com>
2016-07-29 00:15:27 -0500

    add attribute changing methods to Santa class - celebrate_birthday and get_mad_at(reindeer); add gender setter method; add age and ethnicity getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,6 +2,14 @@
 
 class Santa
 
+   def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -10,32 +18,64 @@ class Santa
     puts "That was a good #{cookie_type} cookie!"
   end
 
-  def initialize(gender, ethnicity)
-    puts "Initializing Santa instance..."
-    @gender = gender
-    @ethnicity = ethnicity
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+  def celebrate_birthday
+    @age +=1
   end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking = @reindeer_ranking.insert(-1, @reindeer_ranking.delete(reindeer))
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  #def gender
+  #  @gender
+  #end
+#
+  #def reindeer_ranking
+  #  @reindeer_ranking
+  #end
 
 end
 
 # Driver Code
 
-# kris = Santa.new
-#
-# kris.speak
-#
-# kris.eat_milk_and_cookies("chocolate chip")
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+#kris = Santa.new("Male", "Black")
+#kris.speak
+#kris.eat_milk_and_cookies("chocolate chip")
+#kris.celebrate_birthday
+#puts kris.age
+#puts kris.ethnicity
+#kris.gender = "N/A"
+#kris.get_mad_at("Rudolph")
+#puts kris.gender
+#print kris.reindeer_ranking
 
-p santas
+
+
+
+# santa_hash = {
+#   "female" => "Latina",
+#   "male" => "Japanese",
+#   "gender fluid" => "Black",
+#   "bigender" => "French"
+# }
+#
+# new_santas = []
+# santa_hash.each do |gender, ethnicity|
+#   new_santas << Santa.new(gender, ethnicity)
+# end
+#
+# print new_santas
 
