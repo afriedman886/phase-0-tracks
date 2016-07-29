613db3cc1764c9e2ae3f78b938d0fdce5b9201fa
Amy Friedman <afriedman886@gmail.com>
2016-07-29 16:08:54 -0500

    create todo_list ruby file, add TodoList class, add instance methods to satisfy rspec tests

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,24 @@
+class TodoList
+
+  def initialize(arr)
+    @items = arr
+  end
+
+  def get_items
+    p @items
+  end
+
+  def add_item(new_item)
+    @items = @items.push(new_item)
+  end
+
+  def delete_item(item_to_remove)
+    @items.delete(item_to_remove)
+    @items
+  end
+
+  def get_item(index)
+    @items[index]
+  end
+
+end
