db022883631985309386e3a5844f66758eb6c320
Amy Friedman <afriedman886@gmail.com>
2016-07-28 22:55:50 -0500

    add gender, ethnicity, reindeer ranking, and age attributes to initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,14 +7,35 @@ class Santa
   end
 
   def eat_milk_and_cookies(cookie_type)
-    puts "That was a good #{cookie} cookie!"
+    puts "That was a good #{cookie_type} cookie!"
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
 end
 
+# Driver Code
 
+# kris = Santa.new
+#
+# kris.speak
+#
+# kris.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas
 
