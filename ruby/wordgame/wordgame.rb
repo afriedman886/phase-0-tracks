bb2302447caab94c434c572cf01d156d21634099
Amy Friedman <afriedman886@gmail.com>
2016-07-30 18:43:13 -0500

    add game_won and game_lost methods

--- ruby/wordgame/wordgame.rb
+++ ruby/wordgame/wordgame.rb
@@ -1,10 +1,11 @@
 class Wordgame
 
-#Initialize new game with answer = word/phrase in argument
-#Possible letters to guess = all letters in alphabet
-#Number of guesses allowed = word length + 3 (cuz I'm nice)
+#Initialize new game
+  #Input: word/phrase that needs to be guessed by user
+  #Possible letters user can guess = all letters in alphabet
+  #Number of guesses allowed = word length + 3 (cuz I'm nice)
 
-attr_reader :answer, :guesses_remaining, :letters_to_guess
+attr_reader :answer, :guesses_remaining,:letters_to_guess
 
 def initialize(word)
   @answer = word
@@ -17,17 +18,30 @@ def progress
   @answer.tr(@letters_to_guess.join, "_")
 end
 
+#Update progress once user guesses a letter
+  #IF letter hasn't been guessed on a prior turn, reduce remaining guesses by 1 and display new progress
+  #ELSE ask the user to guess another letter (no deduction from guesses remaining)
 def guess(letter)
-  @letters_to_guess.delete(letter)
+  if @letters_to_guess.include?(letter)
+    @guesses_remaining -= 1
+    @letters_to_guess.delete(letter)
+  else
+    "Oops! Looks like you already tried that letter.  Guess again!"
+  end
+  puts "You have #{@guesses_remaining} guesses left"
   progress
 end
 
-
-#track guesses of user, number of guesses dependent on length of the answer word/phrase
-#repeat guesses do not count against user
-
 #IF the game is won, print congratulatory message
 #ELSE game is lost, print taunting message
+
+def game_won
+  "Great job!!  Those big, beautiful brains of yours just won the game!"
+end
+
+def game_lost
+  "Did you eat a lot of paint chips as a kid?  Better luck next time..."
+end
 
 
 end
--- ruby/wordgame/wordgame_spec.rb
+++ ruby/wordgame/wordgame_spec.rb
@@ -15,13 +15,23 @@ let(:newgame) { Wordgame.new("elephant") }
     expect(newgame.letters_to_guess).to eq ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   end
 
-  it "prints answer with all letters replaced by '_' marks" do
+  it "prints user progress by showing answer word/phrase with all letters not yet guessed replaced by '_' marks" do
     expect(newgame.progress).to eq "________"
   end
 
   it "updates progress after user guesses a letter" do
     newgame.guess("e")
     expect(newgame.progress).to eq "e_e_____"
+    expect(newgame.guesses_remaining).to eq 10
+    expect(newgame.letters_to_guess).to eq ["a", "b", "c", "d",  "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  end
+
+  it "prints congratulatory message when game is won" do
+    expect(newgame.game_won).to eq "Great job!!  Those big, beautiful brains of yours just won the game!"
+  end
+
+  it "prints taunting message when game is lost" do
+    expect(newgame.game_lost).to eq "Did you eat a lot of paint chips as a kid?  Better luck next time..."
   end
 
 end
