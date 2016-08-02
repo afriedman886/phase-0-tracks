53a665a29eeb9dcebae6d4034c0115c13bbacb93
Amy Friedman <afriedman886@gmail.com>
2016-08-02 14:18:50 -0500

    try using concat method to join string

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+// STRING REVERSE
+// Input: string
+// Steps:
+    // Loop through the string, print each character (starting with the last and moving toward the first)
+    // Combine all characters into one return string
+// Return: string printed in reverse order
+
+reverse = function(str) {
+  var arr = str.split("")
+  for (i = (arr.length - 1); i >= 0; i--) {
+    console.log((arr[i]).join(""));}
+    //console.log(arr.join(""));
+
+}
+
+// DRIVER CODE
+reverse("hello");
+//
+//var backwardsAnimal = reverse("elephant");
