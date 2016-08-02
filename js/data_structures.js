1fc277d71feecf1155a8a017c2f9e06d839e86de
Amy Friedman <afriedman886@gmail.com>
2016-08-02 15:44:36 -0500

    create Object with horses/colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,12 @@ horses.push("American Pharoah");
 console.log(colors);
 console.log(horses);
 
+var horseObject = {};
+
+for (var i = 0; i < colors.length; i++) {
+  horseObject[horses[i]] = colors[i];
+}
+
+console.log(horseObject);
+
+
