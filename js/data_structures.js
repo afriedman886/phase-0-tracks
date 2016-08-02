65a29ab111a076a76dc7ff27aadd84284723e944
Amy Friedman <afriedman886@gmail.com>
2016-08-02 15:15:50 -0500

    add colors array and horses array

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["red", "yellow", "turquoise", "sparkle"];
+var horses = ["Seabiscuit", "Secretariat", "Bojack", "Shadowfax"];
+
+colors.push("cerulean");
+horses.push("American Pharoah");
+
+console.log(colors);
+console.log(horses);
+
