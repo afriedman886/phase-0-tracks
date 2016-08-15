2527f50f76259ab3a33a4d86403b6e34944bc80e
Amy Friedman <afriedman886@gmail.com>
2016-08-15 11:12:12 -0500

    syntax cleanup

--- databases/movie_reviewer/movie_reviewer.rb
+++ databases/movie_reviewer/movie_reviewer.rb
@@ -22,24 +22,26 @@ def new_review(db, name, rating, recommended, comments)
 end
 
 
-
-
 # USER INTERFACE
 
 # Ask user about new movie to review
+puts "Welcome to iReview - where the only movie critic's opinion that matters is YOURS!"
 loop do
-  puts "Would you like to add a new movie review? (yes/no)"
+  puts "\nWould you like to add a new movie review? (yes/no)"
   new_movie = gets.chomp
-  break if new_movie == "no"
+  break if new_movie.downcase == "no"
 
   puts "What movie did you see?"
   new_movie = gets.chomp
 
   puts "On a scale from 1-5, what would you rate the movie?"
   user_rating = gets.chomp.to_i
+    until user_rating > 0 && user_rating <= 5
+      puts "Oh boy, your rating is off the charts! No, but seriously, it's off the chart.  Please pick a number between 1-5"
+      user_rating = gets.chomp.to_i
+    end
 
   puts "Would you recommend the movie to a friend? (yes/no)"
-  recommendation_response = ""
   user_recommendation = false
   until user_recommendation
     recommendation_response = gets.chomp
@@ -83,11 +85,4 @@ loop do
   movie_list.each do |movie|
     puts "#{movie["name"]}: #{movie["rating"]} stars '#{movie["comments"]}'  Would recommend: #{movie["recommended"]}"
   end
-
-
-
-  # kittens = db.execute("SELECT * FROM kittens")
-  # kittens.each do |kitten|
-  #  puts "#{kitten['name']} is #{kitten['age']}"
-  # end
 end
