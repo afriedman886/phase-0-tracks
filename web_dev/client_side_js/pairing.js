f24ffc7c0a17d922282521c7ba1c9ff53ca8b8fa
Amy Friedman <afriedman886@gmail.com>
2016-08-19 16:25:51 -0500

    add comments to script; adjust title on html

--- web_dev/client_side_js/pairing.js
+++ web_dev/client_side_js/pairing.js
@@ -1,10 +1,16 @@
+// Release 1
+
 var body = document.getElementsByTagName("body");
 body[0].style.backgroundColor = "blue";
+
+// Create new div element
 
 var el = document.createElement("div");
 el.innerHTML = "<p>Hello world!</p>";
 
 document.body.appendChild(el);
+
+// Release 2
 
 function addBorder(event) {
   event.target.style.border = "3px solid orange";
